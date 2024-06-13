89c898d2abcc870fe2967af8a9ad2d531c7e7d89
Narcisse <itsupport@live.fr>
2024-06-12 18:34:36 -0600

    lightsail

---
+++ output.tf
@@ -0,0 +1,13 @@
+output "public" {
+    value = aws_lightsail_instance.server1.public_ip_address
+  
+}
+
+output "username" {
+  value = aws_lightsail_instance.server1.username
+} 
+
+output "ssh-commad" {
+  value = "ssh -i ${local_file.ssh_key.filename} ${aws_lightsail_instance.server1.username}@${aws_lightsail_instance.server1.public_ip_address}"
+}
+
